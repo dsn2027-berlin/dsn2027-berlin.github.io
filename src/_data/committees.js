4309128f5ae59a1f18ceba252c94c9ca93216c57
Shang Zhihao <zhihao.shang@gmail.com>
2026-06-27 11:52:46 +0200

    Revise committees page

---
+++ src/_data/committees.js
@@ -0,0 +1,139 @@
+export const roles = [
+  {
+    role: "General Chairs",
+    people: [
+      {
+        name: "Katinka Wolter",
+        url: "https://www.mi.fu-berlin.de/inf/groups/ag-dds/staff/wolter.html",
+        institution: "Freie Universität Berlin",
+        country: "Germany",
+        photo: "/assets/committees/katinka-wolter.png",
+      },
+      {
+        name: "Marco Paolieri",
+        url: "https://qed.usc.edu/paolieri/",
+        institution: "University of Southern California",
+        country: "USA",
+        photo: "/assets/committees/marco-paolieri.png",
+      },
+    ],
+    status: "Listed",
+  },
+  {
+    role: "Program Committee Chairs",
+    people: [
+      {
+        name: "Rüdiger Kapitza",
+        url: "https://sys.cs.fau.de/person/kapitza",
+        institution: "Friedrich Alexander Universität",
+        country: "Germany",
+      },
+      {
+        name: "Homa Alemzadeh",
+        url: "https://engineering.virginia.edu/faculty/homa-alemzadeh",
+        institution: "University of Virginia",
+        country: "USA",
+      },
+    ],
+    status: "In progress",
+  },
+  { role: "Industry Track Chairs", people: [], status: "To be announced" },
+  { role: "Artifact Chairs", people: [], status: "To be announced" },
+  { role: "Disrupt 2027", people: [], status: "To be announced" },
+  { role: "Workshop Chairs", people: [], status: "To be announced" },
+  { role: "Poster Chairs", people: [], status: "To be announced" },
+  { role: "Doctoral Forum Chairs", people: [], status: "To be announced" },
+  { role: "Tutorial Chairs", people: [], status: "To be announced" },
+  { role: "Mentoring Program Chairs", people: [], status: "To be announced" },
+  { role: "Publication Chairs", people: [], status: "To be announced" },
+  { role: "Publicity Chairs", people: [], status: "To be announced" },
+  { role: "Social Media Chair", people: [], status: "To be announced" },
+  {
+    role: "Finance and Student Travel Grant Chairs",
+    people: [],
+    status: "To be announced",
+  },
+  {
+    role: "Webmaster Chair",
+    people: [
+      {
+        name: "Zhihao Shang",
+        institution: "SSE - Secure Systems Engineering GmbH",
+        country: "Germany",
+      },
+    ],
+    status: "Listed",
+  },
+  { role: "Sponsorship Chairs", people: [], status: "To be announced" },
+  {
+    role: "Visa and Registration Chair",
+    people: [
+      {
+        name: "Justus Purat",
+        url: "https://www.mi.fu-berlin.de/en/inf/groups/ag-dds/staff/purat_justus.html",
+        institution: "Freie Universität Berlin",
+        country: "Germany",
+      },
+    ],
+    status: "Listed",
+  },
+  {
+    role: "Local Arrangement Chair",
+    people: [
+      {
+        name: "Yanhua Chen",
+        institution: "Lanzhou University",
+        country: "China",
+      },
+    ],
+    status: "Listed",
+  },
+];
+
+export const potentialContributors = [
+  "Andrea Ceccarelli",
+  "Domenico Cotroneo",
+  "Elena Trubytsina",
+  "Peter Popov",
+  "Karthik Pattabiraman",
+  "Behrooz Sangchoolie",
+  "Sara Bouchenak",
+];
+
+export const planningIdeas = [
+  {
+    area: "Industry",
+    people: [
+      "Marco Platania, AT&T, USA",
+      "Behrooz Sangchoolie, RISE, Sweden",
+      "Michael Paulitsch, Intel, Germany",
+      "Raffaele Della Corte, Federico II University of Naples, Italy",
+      "Subho Banerjee",
+    ],
+    note: "Ideas from Cristina and Karthik; includes references from DSN 2025 planning.",
+  },
+  {
+    area: "Disrupt",
+    people: ["Marcus Volp", "Andrea Bondavalli", "Andrea Ceccarelli"],
+  },
+  {
+    area: "Artifact",
+    people: [
+      "Carmine Cesarano, University of Naples Federico II",
+      "Zitao Chen",
+      "Christoph Fetzer and students",
+    ],
+  },
+  {
+    area: "Workshop",
+    people: ["Sara Bouchenak", "Paolo Rech", "Ghassan Karame"],
+  },
+  {
+    area: "Tutorial",
+    people: [
+      "Horst Schrimeier",
+      "Pramod Bhatotia, TU Munich",
+      "Jean-Pierre Seiffert",
+    ],
+  },
+];
